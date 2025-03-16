23105f8cb029764b33114eaac7bd3a0bb043b80f
Silas217209 <80594111+Silas217209@users.noreply.github.com>
2025-03-16 23:54:26 +0100

    updates

---
+++ README.md
@@ -0,0 +1,40 @@
+# Lesefuchs
+
+## Datenbank-Grundlage
+
+- [ ] Alle vorhandenen Medien digital erfassen
+    - [ ] Bücher
+    - [ ] Spiele
+    - [ ] DVDs
+    - [ ] CDs
+    - [ ] Computerspiele (mit Altersbeschränkung)
+    - [ ] Anzahl vorhandener Exemplare je Medientyp
+- [x] Jedes Medium erhält eine eindeutige ID
+- [x] Jedes Medium hat einen Status: **ausgeliehen** oder **verfügbar**
+- [x] Liste aller Kunden mit relevanten Daten
+    - [x] Alter zur Altersfreigabe-Prüfung
+    - [ ] Name, Vorname, Adresse
+    - [ ] E-Mail und Telefonnummer
+
+## Leihvorgänge
+
+- [x] Leihvorgänge digital speichern
+- [x] Prüfen, ob ein Medium noch verfügbar ist, bevor es ausgeliehen wird
+- [ ] Hinweis bei überzogener Leihzeit
+- [ ] Unterschiedliche Ausleihdauer pro Medientyp
+    - [ ] Filme: kurze Leihfrist
+    - [ ] Spiele: mittlere Leihfrist
+    - [ ] Bücher: längere Leihfrist
+
+## Kundenverwaltung
+
+- [ ] Mindestens 10 Kunden initial in der Datenbank
+- [ ] Dynamische Liste der Kunden
+- [ ] Möglichkeit, säumige Kunden zu informieren
+- [ ] Tägliche Überprüfung der Kundendatenbank auf überzogene Leihfristen
+
+## Verwaltungsübersicht
+
+- [x] Bibliothek kann alle aktuell ausgeliehenen Medien einsehen
+- [x] Medien, Kunden und Leihvorgänge verwalten
+- [ ] Benutzeroberfläche zur Verwaltung (z. B. Konsolenanwendung oder grafische Oberfläche)
--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,35 @@
-/*
- * Lesefuchs
- * Ein Bibliothek in Ihrer Region möchte ein neues Buchungssystem installieren. So sollen sowohl
- * alle Medien als auch alle Kunden und Leihvorgänge digital digital verarbeitet werden. Sie sollen
- * helfen, dieses System objektorientiert umzusetzen.
- * Grundlage für die Datenbank sind die vorhandenen Medien, diese gibt es als Bücher, Spiele und
- * DVDs, CDs und Computerspiele (mit Altersbeschränkung) und Anzahl vorhandener Medien eines
- * Typs. Jedes Medium hat eine eigene ID und einen Zustand ausgeliehen oder nicht.
- * Es gibt eine Liste, die alle Kunden enthält, dabei ist das Alter wichtig, damit Kinder keine Medien
- * ausleihen können, die nicht für sie zu gelassen sind.
- * Es gibt eine Liste, die die Medien enthält.
- * Die Leihvorgänge müssen gespeichert werden und beim Ausleihen beachtet werden, dass noch
- * eine Exemplar des Mediums vorhanden ist. Bei der Rückgabe muss die Anzahl ebenfalls beachtet
- * werden und eine Hinweis zu überzogenen Leihzeit .
- * Die Höchstdauer der Ausleihe ist bei verschiedenen Medien unterschiedlich, Filme kürzer als
- * Spiele. Bücher dürfen länger behalten werden.
- * Die Datenbank enthält eine Anzahl von mindestens 10 Kunden (als dynamische Liste, gern auch
- * objektorientiert implementiert, aber kein Muss), die spezielle Personen (Name, Vorname, Adresse)
- * mit Email und Telefonnummern sind. Es muss möglich sein säumige Kunden zu informieren, dass
- * sie Strafe zahlen müssen, wenn sie überzogen haben.
- * Täglich wird die Kundendatenbank einmal auf säumige Kunden überprüft.
- * Die Bibliothek möchte eine Übersicht über alle aktuell ausgeliehenen Medien haben. Es sollte
- * möglich sein, Medien, Kunden und Leihvorgänge über eine Benutzerober äche (z. B.
- * Konsolenanwendung oder gra sche Ober äche) zu verwalten.
- */
-
-#include <unordered_map>
+#include <ctime>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 class Medium {
 public:
-    Medium(int id) : m_id(id) {
-    }
+    explicit Medium(int id) : m_id(id) {}
+    virtual ~Medium() = default;
 
-    int id() const {
-        return m_id;
-    }
+    int id() const { return m_id; }
 
-    virtual bool darfAusleihen(int alter) { return true; }
+    virtual bool darfAusleihen(int /*alter*/) { return true; }
+
+    virtual int maxLeihDauer() const { return 30; };
+
+    bool ausgeliehen() const { return m_ausgeliehen; };
+
+    void setAusgeliehen(bool ausgeliehen) { m_ausgeliehen = ausgeliehen; };
 
 private:
     int m_id;
+    bool m_ausgeliehen = false;
 };
 
 class Kunde {
 public:
-    Kunde(int id): m_id(id) {
-    };
+    Kunde(int id, int alter) : m_id(id), m_alter(alter) {}
 
-    int id() const {
-        return m_id;
-    }
+    int id() const { return m_id; }
 
-    int alter() const {
-        return m_alter;
-    }
+    int alter() const { return m_alter; }
 
 private:
     int m_id;
@@ -64,32 +39,41 @@ private:
 
 class Leihvorgang {
 public:
-    Leihvorgang(int kundenId, int mediumId): m_kundenId(kundenId), m_mediumId(mediumId), m_ausleihDatum(time(nullptr)) {
-    };
+    Leihvorgang(const Kunde *kundenId, const Medium *mediumId) :
+        m_kunden(kundenId), m_medium(mediumId), m_ausleihDatum(time(nullptr)) {};
 
-    int kundenId() const {
-        return m_kundenId;
+    const Kunde *pKunde() const { return m_kunden; }
+
+    const Medium *pMedium() const { return m_medium; }
+
+    bool istUeberfaellig() const {
+        time_t jetzt = time(nullptr);
+        double diffsec = difftime(jetzt, m_ausleihDatum);
+        return diffsec > m_medium->maxLeihDauer() * 60 * 60 * 24;
     }
 
-    int mediumId() const {
-        return m_mediumId;
+    int ueberfaelligTage() {
+        if (!istUeberfaellig()) {
+            return 0;
+        }
+
+        time_t jetzt = time(nullptr);
+        double diffsec = difftime(jetzt, m_ausleihDatum);
+        int diffDays = static_cast<int>(diffsec / (60 * 60 * 24));
+        return diffDays - m_medium->maxLeihDauer();
     }
 
 private:
-    int m_kundenId;
-    int m_mediumId;
+    const Kunde *m_kunden;
+    const Medium *m_medium;
     time_t m_ausleihDatum;
 };
 
 class Bibliothek {
 public:
-    void mediumHinzufuegen(Medium *medium) {
-        m_medien.emplace(medium->id(), medium);
-    }
+    void mediumHinzufuegen(Medium *medium) { m_medien.emplace(medium->id(), medium); }
 
-    void kundeHinzufuegen(Kunde *kunde) {
-        m_kunden.emplace(kunde->id(), kunde);
-    }
+    void kundeHinzufuegen(Kunde *kunde) { m_kunden.emplace(kunde->id(), kunde); }
 
     bool ausleihen(Kunde *kunde, Medium *medium) {
         if (m_kunden.find(kunde->id()) == m_kunden.end()) {
@@ -104,7 +88,12 @@ public:
             return false;
         }
 
-        m_leihvorgaenge.emplace_back(kunde->id(), medium->id());
+        if (medium->ausgeliehen()) {
+            return false;
+        }
+
+        medium->setAusgeliehen(true);
+        m_leihvorgaenge.emplace_back(kunde, medium);
 
         return true;
     }
@@ -119,17 +108,33 @@ public:
         }
 
         for (auto it = m_leihvorgaenge.begin(); it < m_leihvorgaenge.end(); ++it) {
-            if (it.base()->kundenId() != kunde->id()) {
+            if (it.base()->pKunde()->id() != kunde->id()) {
                 continue;
             }
 
-            if (it.base()->mediumId() != medium->id()) {
+            if (it.base()->pMedium()->id() != medium->id()) {
                 continue;
             }
 
+            medium->setAusgeliehen(false);
             m_leihvorgaenge.erase(it);
             return true;
         }
+
+        return false;
+    }
+
+    // return an array with pointers to Kunde
+    std::vector<const Kunde *> sauemigeKunden() {
+        std::vector<const Kunde *> sauemigeKunden;
+        for (auto l: m_leihvorgaenge) {
+            if (l.istUeberfaellig()) {
+                std::cout << "Kunde " << l.pKunde()->id() << " hat Medium " << l.pMedium()->id() << " überfällig!\n";
+                sauemigeKunden.emplace_back(l.pKunde());
+            }
+        }
+
+        return sauemigeKunden;
     }
 
     void zeigeKunden() {
@@ -147,7 +152,7 @@ public:
     void zeigeVerleihe() {
         std::cout << "Kunde <-> Medium\n";
         for (const auto &l: m_leihvorgaenge) {
-            std::cout << l.kundenId() << " <-> " << l.mediumId() << "\n";
+            std::cout << l.pKunde()->id() << " <-> " << l.pMedium()->id() << "\n";
         }
     }
 
@@ -161,13 +166,13 @@ private:
 int main() {
     Bibliothek bib;
 
-    auto k0 = new Kunde(0);
-    auto k1 = new Kunde(1);
-    auto k2 = new Kunde(2);
+    auto *k0 = new Kunde(0, 21);
+    auto *k1 = new Kunde(1, 22);
+    auto *k2 = new Kunde(2, 17);
 
-    auto m0 = new Medium(0);
-    auto m1 = new Medium(1);
-    auto m2 = new Medium(2);
+    auto *m0 = new Medium(0);
+    auto *m1 = new Medium(1);
+    auto *m2 = new Medium(2);
 
     bib.kundeHinzufuegen(k0);
     bib.kundeHinzufuegen(k1);
@@ -177,7 +182,9 @@ int main() {
     bib.mediumHinzufuegen(m1);
     bib.mediumHinzufuegen(m2);
 
+    bib.ausleihen(k0, m0);
     bib.ausleihen(k0, m1);
+    bib.ausleihen(k0, m2);
 
     bib.zeigeKunden();
     std::cout << "\n";
@@ -188,6 +195,7 @@ int main() {
     bib.zurueckgeben(k0, m1);
     std::cout << "\n";
     bib.zeigeVerleihe();
+    bib.sauemigeKunden();
 
     delete k0;
     delete k1;
